1483f81781b788ecc7daa4a549f9983e3a6e0725
amitlzkpa <amit.lzkpa@gmail.com>
2020-02-05 03:41:51 +0100

    copy over documenting script from web project

--- docs.js
+++ docs.js
@@ -1,9 +1,61 @@
-// const fs = require('fs')
+const fs = require('fs')
+const glslfuncs = require('./src/composable-glsl-functions.js')
 
-// let glslTxt = fs.readFileSync('./src/glsl-transforms.js').toString()
-// console.log(glslTxt);
+// const [,, outPath] = process.argv
+const outPath = './docs/funcs.md'
 
+const categories = {}
+const argtypes = {}
 
-const t = require('./src/glsl-transforms.js')
-console.log(t)
+Object.keys(glslfuncs).forEach((k) => {
+  const f = glslfuncs[k]
+  const categoryName = f.type.charAt(0).toUpperCase() + f.type.slice(1)
+  categories[categoryName] = categories[categoryName] || []
+  categories[categoryName].push(k)
+  const inputs = f.inputs || []
+  inputs.forEach((i) => {
+    argtypes[i.type] = i.type
+  })
+})
+
+const createlist = (items) => items.map((i) => `* ${i}`).join('\n')
+
+const createFuncDoc = (fname) => {
+  const title = `#### ${fname}`
+  const argnames = (glslfuncs[fname].inputs || []).map(i => `${i.name} :: ${i.type}`)
+
+  let args
+  if (argnames.length > 1) {
+    args = `#### Args\n${createlist(argnames)}`
+  } else {
+    args = 'No Args'
+  }
+
+  return `${title}\n\n${args}\n`
+}
+
+const createCategory = (cname) => `### ${cname}
+
+${categories[cname].map(createFuncDoc).join('\n')}
+`
+
+const output = `# Functions
+
+## Argument Types
+
+${createlist(Object.keys(argtypes).sort())}
+
+## Functions
+
+${Object.keys(categories).sort().map(createCategory).join('\n')}
+
+`
+
+fs.writeFile(outPath, output, function (err) {
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log('The file was saved!')
+})
 
