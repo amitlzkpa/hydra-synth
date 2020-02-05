041a8a276896b143502e34498a5b05db15f9eeab
amitlzkpa <amit.lzkpa@gmail.com>
2020-02-05 03:27:44 +0100

    setup docs.js script

---
+++ docs.js
@@ -0,0 +1,9 @@
+// const fs = require('fs')
+
+// let glslTxt = fs.readFileSync('./src/glsl-transforms.js').toString()
+// console.log(glslTxt);
+
+
+const t = require('./src/glsl-transforms.js')
+console.log(t)
+
